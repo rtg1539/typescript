cf183c8921e016b619a5c53f31dc6a7edd6f49aa
shkim <rtg1539@naver.com>
2021-07-18 17:11:35 +0900

    optional-chaining

---
+++ src/etc/optional-chaining.ts
@@ -0,0 +1,14 @@
+// Error - TS2532: Object is possibly 'undefined'.
+function toString(str: string | undefined) {
+  return str.toString();
+}
+
+// Type Assertion
+function toString(str: string | undefined) {
+  return (str as string).toString();
+}
+
+// Optional Chaining
+function toString(str: string | undefined) {
+  return str?.toString();
+}
