e989f0bf83368f3693421dac57209c1bf00dbd54
shkim <rtg1539@naver.com>
2021-07-18 16:39:55 +0900

    generic - conditional

---
+++ src/generic/conditional.ts
@@ -0,0 +1,36 @@
+type U = string | number | boolean;
+
+// type 식별자 = 타입 구현
+type MyType<T> = T extends U ? string : never;
+
+// interface 식별자 { 타입 구현 }
+interface IUser<T> {
+  name: string,
+  age: T extends U ? number : never
+}
+
+// `T`는 `boolean` 타입으로 제한.
+interface IUser<T extends boolean> {
+  name: string,
+  age: T extends true ? string : number, // `T`의 타입이 `true`인 경우 `string` 반환, 아닌 경우 `number` 반환.
+  isString: T
+}
+
+const str: IUser<true> = {
+  name: 'Neo',
+  age: '12', // String
+  isString: true
+}
+const num: IUser<false> = {
+  name: 'Lewis',
+  age: 12, // Number
+  isString: false
+}
+
+type MyType<T> =
+  T extends string ? 'Str' :
+    T extends number ? 'Num' :
+      T extends boolean ? 'Boo' :
+        T extends undefined ? 'Und' :
+          T extends null ? 'Nul' :
+            'Obj';
